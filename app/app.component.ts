a4494da504e06702233082bf32358677af16c886
trafalgarDWaterLaw <mohitparitosh@gmail.com>
2016-11-13 04:20:53 +0530

    Sorting Functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -19,11 +19,44 @@ export class AppComponent implements OnInit{
             console.log('*******************************');
             console.log(this.userDetailArr);
             console.log('*******************************');
-            this.serviceError = this.gitService.errorMsg;
+            /*this.serviceError = this.gitService.errorMsg;
             if(this.serviceError){
                 console.log('******service Error occured is *****');
                 console.log(this.serviceError);
-            }
+            }*/
         });
+    }
+     sortByFollowers(){
+             this.userDetailArr.sort((n1,n2) => n2.followers - n1.followers);
+    }
+    sortByLocation(){
+            this.userDetailArr.sort((n1,n2) => {
+            if (n1.location > n2.location) {
+                return 1;
+            }
+
+            if (n1.location < n2.location) {
+                return -1;
+            }
+            return 0;
+        });
+    }
+    sortByName(){
+            this.userDetailArr.sort((n1,n2) => {
+            if (n1.name > n2.name) {
+                return 1;
+            }
+
+            if (n1.name < n2.name) {
+                return -1;
+            }
+            return 0;
+        });
+    }
+    removeThisElement(removeUser:User){
+        let idx = this.userDetailArr.indexOf(removeUser);
+        if (idx != -1) {
+                return this.userDetailArr.splice(idx, 1); 
+            }
     }
 }
